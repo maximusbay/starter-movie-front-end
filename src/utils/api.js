8358ee65d9740437873b0c1a8050d5f21ee66a89
Maximus Bay <mackzbay@gmail.com>
2023-09-20 02:30:20 -0700

    url correction

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://welovemovies-backend-g08a.onrender.com";
+const API_BASE_URL = "https://welovemovies-backend-g08a.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
